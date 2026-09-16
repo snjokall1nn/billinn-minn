126cf7f3a13231debd9c9b4fb1ec13528d68e6fc
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 00:48:03 +0000

    Add fuel endpoint config

---
+++ config.js
@@ -0,0 +1,3 @@
+// Paste the deployed Google Apps Script Web App URL here.
+// Example: window.BILLINN_SCRIPT_URL = 'https://script.google.com/macros/s/ABC123/exec';
+window.BILLINN_SCRIPT_URL = '';
