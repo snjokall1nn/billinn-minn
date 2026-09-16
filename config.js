fdf6be539c577a9cf09aba0809713d4c019f0e09
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 01:26:32 +0000

    Connect Bíllinn minn to Apps Script

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Paste the deployed Google Apps Script Web App URL here.
-// Example: window.BILLINN_SCRIPT_URL = 'https://script.google.com/macros/s/ABC123/exec';
-window.BILLINN_SCRIPT_URL = '';
+// Deployed Google Apps Script Web App URL.
+window.BILLINN_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwjKE7Z2ceHGz07xwgh_zuDkKarxJhzUXaM6NDVqgEjahdTmHA4LAwB4xxOAYsG_qI/exec';
